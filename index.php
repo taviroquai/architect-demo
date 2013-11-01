0d51d666b77d49132172fe91a780e39672414f4c
Marco Afonso <mafonso333@gmail.com>
2013-11-01 00:19:54 +0000

    Update demo to Architect 0.0.12

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
 
-// IMPORTANT!!! Define base path constant
-define('BASE_PATH', __DIR__);
-define('ARCH_PATH', '/var/www/architectphp');
-
-// IMPORTANT!!! Require architect framework autoloader
-require_once ARCH_PATH.'/src/autoload.php';
-require_once ARCH_PATH.'/src/aliases.php';
+// IMPORTANT!!! Require composer autoload
+require_once 'vendor/autoload.php';
 
 // IMPORTANT!!! Give configuration file and run application
 $config = "config/development.xml";
-$app = \Arch\App::Instance($config);
-
-// load default theme
-$app->loadTheme(__DIR__.'/theme/default');
-
-// run application
-$app->run();
+$app = \Arch\App::Instance($config)
+        ->aliases()
+        ->run();
