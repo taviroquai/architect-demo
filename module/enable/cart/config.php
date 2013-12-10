8574b8503a9010e230662843a4fd628e9ea4df68
Marco Afonso <mafonso333@gmail.com>
2013-12-10 19:05:04 +0000

    Use new cart model

--- module/enable/cart/config.php
+++ module/enable/cart/config.php
@@ -5,22 +5,31 @@ r('/demo/cart', function() {
     // demo of the shopping cart
     $cart = view()->createCart();
     $cart->set('checkoutUrl', u('/demo/cart'));
-    // if you use other item attributes please extend Model_Cart, View_Cart, 
-    // copy template theme/default/cart.php and change attributes
-    $item = (object) array('name' => 'Product1', 'price' => 30, 'tax' => 0.21);
-    $cart->model->insertItem($item, 1, 2); // inserts on id 1 and quantity 2
-    $cart->model->updateQuantity(1, 3); // updates item 1 quantity to 3
-    $cart->model->updateShippingCost(5); // updates shipping cost to 5
+    
+    if (!$cart->getModel()->loadCart(app()->getSession())) {
+        $item = (object) array(
+            'name' => 'Product1',
+            'price' => 30,
+            'tax' => 0.21
+        );
+        // inserts on id 1 and quantity 2
+        $cart->getModel()->insertItem($item, 1, 2);
+    }
+    
+    if (i('shipping') == 'Standard') {
+        $cart->getModel()->updateShippingCost(5);
+    }
     
     if (i('del')) {
-        $cart->model->updateQuantity(i('del'), 0);
+        $cart->getModel()->updateQuantity(i('del'), 0);
     }
     if (i('quantity')) {
         $items = i('quantity');
         foreach (array_keys($items) as $id) {
-            $cart->model->updateQuantity($id, (int) $items[$id]);
+            $cart->getModel()->updateQuantity($id, (int) $items[$id]);
         }
     }
+    $cart->getModel()->saveCart(app()->getSession());
 
     $layout = l(__DIR__.'/theme/template.php')->addContent($cart);
     c($layout);
